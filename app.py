4ca2354b5f8452f6e4b3732be0169fe6bc55c456
JEHR95 <jhernandez260895@gmail.com>
2025-09-08 12:46:43 -0600

    Modificación en posición de encabezado y botones

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import streamlit as st
 import plotly_express as px
 
 df_vehicles = pd.read_csv('vehicles_us.csv') #carga los datos del df
-hist_button = st.button('Generar Histograma') #crea un botón para ejecutar el código del histograma
-scatter_button = st.button('Generar Dispersión') #crea un botón para ejecutar el código del grafico de dispersión
 
 st.header('Aplicación de vehículos') #encabezado
+scatter_button = st.button('Generar Dispersión') #crea un botón para ejecutar el código del grafico de dispersión
+hist_button = st.button('Generar Histograma') #crea un botón para ejecutar el código del histograma
+
 
 if hist_button: # al hacer clic en el botón
     # escribe un mensaje
@@ -27,5 +28,5 @@ if scatter_button:
                              y='price'
                              )
     #muestra el gráfico Plotly interactivo
-    st.plotly_chart(fig_hist, use_container_width=True)
+    st.plotly_chart(fig_scatter, use_container_width=True)
 
