ffa0ef15d0691a57ea0d4ccdcdcc112589354f54
JEHR95 <jhernandez260895@gmail.com>
2025-09-08 17:10:26 -0600

    Versión de entrega de readme y app.py

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Mediante el uso conjunto de **VScode**, **GitHub** y **Render.com** se logra car
 para consulta de información con gráficos interactivos en la web, generando un enlace que permite
 desde cualquier dispositivo visitar dicha aplicación web facilitando el análisis para cada usuario
 de la información que sea de su interés respecto al tema a tratar.
+
+La aplicación web está diseñada para que al seleccionar las casillas se generen los gráficos correspondientes
+que permitirán visualizar la información de manera general o más a detalle realizando zoom al interior del gráfico
--- app.py
+++ app.py
@@ -4,13 +4,17 @@ import plotly_express as px
 
 df_vehicles = pd.read_csv('vehicles_us.csv') #carga los datos del df
 
-st.header('Aplicación de vehículos') #encabezado
-# crea casillas de verificación
-build_histogram = st.checkbox('¡Hagámos un histograma!')
-build_scatter = st.checkbox('¡Veamos la dispersión!')
+st.header('Ventana de datos') #encabezado
+# Casilla de verificación
+build_df = st.checkbox('Visualizador de datos')
+if build_df: #si la casilla está seleccionada...
+    # Genera una ventana de visualización interactiva del Dataframe
+    df_window = st.dataframe(df_vehicles)
 
-if build_histogram: #si la casilla está seleccionada...
-    st.write('Construiremos un histograma para la columna odómetro')
+st.header('Densidades por kilometraje')
+build_histogram = st.checkbox('¡Hagámos el histograma!')
+if build_histogram:
+    st.write('Histograma por kilometraje recorrido')
     #crea un histograma
     fig_hist = px.histogram(df_vehicles,
                             x='odometer'
@@ -18,8 +22,10 @@ if build_histogram: #si la casilla está seleccionada...
     #muestra un gráfico Plotly interactivo
     st.plotly_chart(fig_hist, use_container_width=True)
 
+st.header('Relación Precio/Kilometraje')
+build_scatter = st.checkbox('¡Veamos la dispersión!')
 if build_scatter: #si la casilla está seleccionada...
-    st.write('Construiremos un gráfico de dispersión para las columnas odómetro y price')
+    st.write('Gráfco de dispersión entre Precio y Kilometraje')
     #crea un gráfico de dispersión
     fig_scatter = px.scatter(df_vehicles,
                              x='odometer',
